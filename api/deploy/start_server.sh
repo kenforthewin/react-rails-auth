c135f399091eb46b418dc8ddda2f5a6d721f4891
Kenny <kenny@nurelm.com>
2017-11-25 14:20:31 -0500

    get the rails app serving again

--- api/deploy/start_server.sh
+++ api/deploy/start_server.sh
@@ -10,6 +10,6 @@ bundle exec rake db:create RAILS_ENV=$RAILS_ENV
 bundle exec rake db:migrate RAILS_ENV=$RAILS_ENV
 
 if [ "$RAILS_ENV" = "development" ]; then
-  bundle exec rails s -b 0.0.0.0 -p 3001 -d
+  bundle exec puma -d
   exec tail -f log/development.log
 fi
